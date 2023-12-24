9f32e6f4ec0eb52be12ab8f69ee08159f867f047
Robert Strojny <robert.strojny95@gmail.com>
2023-12-24 15:59:26 +0100

    Add canBePicked property to BaseBox class

--- app/src/models/BaseBox.ts
+++ app/src/models/BaseBox.ts
@@ -1,17 +1,16 @@
 import { Mesh, MeshBuilder, Scene } from "@babylonjs/core";
 
 class BaseBox extends Mesh {
+  public canBePicked: boolean;
+
   constructor(name: string, scene: Scene) {
     super(name, scene);
+    this.canBePicked = true;
   }
 
   public static create(name: string, size: number, scene: Scene): Mesh {
-    return MeshBuilder.CreateBox(name, { size: size }, scene);
+    return MeshBuilder.CreateBox(name, { size: size }, scene) as BaseBox;
   }
-
-  //   public canBePicked(): boolean {
-  //     return true;
-  //   }
 }
 
 export { BaseBox };
