e2322b7f42523306704d44de034b767dbf474748
Robert Strojny <robert.strojny95@gmail.com>
2023-12-25 16:59:34 +0100

    Add winning combinations and positions for Tic Tac Toe game

--- app/src/app.ts
+++ app/src/app.ts
@@ -21,6 +21,20 @@ class App {
   private boxes: BaseBox[] = [];
   private players: Player[] = [];
   private currentPlayer: number = 0;
+  private winningCombinations: number[] = [
+    123, 456, 789, 147, 258, 369, 159, 357,
+  ];
+  private positions: { [key: number]: Vector3 } = {
+    1: new Vector3(-1, 1, -1),
+    2: new Vector3(0, 1, -1),
+    3: new Vector3(1, 1, -1),
+    4: new Vector3(-1, 0, -1),
+    5: new Vector3(0, 0, -1),
+    6: new Vector3(1, 0, -1),
+    7: new Vector3(-1, -1, -1),
+    8: new Vector3(0, -1, -1),
+    9: new Vector3(1, -1, -1),
+  };
 
   constructor() {
     // create the canvas html element and attach it to the webpage
@@ -39,37 +53,47 @@ class App {
     scene.clearColor = new Color4(0, 0, 0, 0);
 
     // create players
-    this.players = Player.createPlayers(
-      ["Player 1", "Player 2"],
-      ["red", "blue"]
-    );
+    this.players.push(new Player("Player 1", 0, "red"));
+    this.players.push(new Player("Player 2", 0, "blue"));
 
-    var positions = {
-      1: new Vector3(-1, 1, -1),
-      2: new Vector3(0, 1, -1),
-      3: new Vector3(1, 1, -1),
-      4: new Vector3(-1, 0, -1),
-      5: new Vector3(0, 0, -1),
-      6: new Vector3(1, 0, -1),
-      7: new Vector3(-1, -1, -1),
-      8: new Vector3(0, -1, -1),
-      9: new Vector3(1, -1, -1),
-    };
-
-    for (var i in positions) {
-      var box: BaseBox = BaseBox.create("box", 0.6, scene) as BaseBox;
-      box.position = positions[i];
+    // create boxes
+    for (var i in this.positions) {
+      var box: BaseBox = new BaseBox("box", scene, 0.6);
+      box.position = this.positions[i];
 
       var boxMaterial: BoxBaseMaterial = new BoxBaseMaterial(
-        "box",
+        "boxMaterial",
         scene,
         "😶‍🌫️",
         "red"
       );
-
       box.material = boxMaterial;
+
+      // Set up the ActionManager for the box
+      box.actionManager = new ActionManager(scene);
+      box.actionManager.registerAction(
+        new ExecuteCodeAction(ActionManager.OnPickTrigger, () => {
+          if (box.canBePicked === false) {
+            return;
+          }
+
+          if (this.currentPlayer === 0) {
+            box.material = new BoxBaseMaterial("box", scene, "X", "red");
+            this.currentPlayer = 1;
+          } else {
+            box.material = new BoxBaseMaterial("box", scene, "O", "blue");
+            this.currentPlayer = 0;
+          }
+          box.canBePicked = false;
+        })
+      );
+
+      console.log(box);
       this.boxes.push(box);
     }
+
+    // console.log(box.material);
+    console.log(boxMaterial);
 
     var camera: ArcRotateCamera = new ArcRotateCamera(
       "Camera",
@@ -104,8 +128,15 @@ class App {
     // run the main render loop
     engine.runRenderLoop(() => {
       // Game logic goes here
-      this.actionManagerForClickingBoxes(scene, this.boxes, this.currentPlayer);
-      this.updateCurrentPlayerDisplay();
+      if (this.isThereAWinner(scene, this.boxes, this.winningCombinations)) {
+      } else {
+        this.actionManagerForClickingBoxes(
+          scene,
+          this.boxes,
+          this.currentPlayer
+        );
+        this.updateCurrentPlayerDisplay();
+      }
       scene.render();
     });
   }
@@ -142,6 +173,43 @@ class App {
     currentPlayerElement.textContent = `Current player: ${
       this.players[this.currentPlayer].name
     }`;
+  }
+
+  private isThereAWinner(
+    scene: Scene,
+    boxes: BaseBox[],
+    winningCombinations: number[]
+  ): boolean {
+    for (let combination of winningCombinations) {
+      let box1 = boxes[+combination.toString()[0] - 1];
+      let box2 = boxes[+combination.toString()[1] - 1];
+      let box3 = boxes[+combination.toString()[2] - 1];
+
+      // if (
+      //   box1.material.getActiveTextures === box2.material.text &&
+      //   box2.material.text === box3.material.text
+      // ) {
+      //   box1.material = new BoxBaseMaterial(
+      //     "box",
+      //     scene,
+      //     box1.material.text,
+      //     "green"
+      //   );
+      //   box2.material = new BoxBaseMaterial(
+      //     "box",
+      //     scene,
+      //     box2.material.text,
+      //     "green"
+      //   );
+      //   box3.material = new BoxBaseMaterial(
+      //     "box",
+      //     scene,
+      //     box3.material.text,
+      //     "green"
+      //   );
+      // }
+    }
+    return false;
   }
 }
 
